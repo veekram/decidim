fd3e393e36bd028af155e9b12d9db1e4ec8545ec
Agustí B.R <agusti.br@coditramuntana.com>
2018-07-23 17:30:35 +0200

    Fix tos tests

--- decidim-core/lib/decidim/core/test/factories.rb
+++ decidim-core/lib/decidim/core/test/factories.rb
@@ -71,11 +71,13 @@ FactoryBot.define do
     enable_omnipresent_banner false
     tos_version { Time.current }
 
+    after(:create) do |organization|
+      tos_page = Decidim::StaticPage.find_by(slug: "terms-and-conditions", organization: organization)
+      tos_page = create(:static_page, :tos, organization: organization) if tos_page.nil?
+      organization.tos_version = tos_page.updated_at
+    end
+
     trait :with_tos do
-      after(:create) do |organization|
-        tos_page = Decidim::StaticPage.find_by(slug: "terms-and-conditions", organization: organization)
-        create(:static_page, :tos, organization: organization) if tos_page.nil?
-      end
     end
   end
 
